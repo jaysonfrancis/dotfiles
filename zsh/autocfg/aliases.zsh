d1d9c9215f91ec1d7db723039ab15532deadb6a9
Jayson Francis <jaysonfrancis@users.noreply.github.com>
2025-03-26 16:20:33 -0700

    update

--- zsh/autocfg/aliases.zsh
+++ zsh/autocfg/aliases.zsh
@@ -1,8 +1,8 @@
 # aliases.zsh 
 
 alias LL="eza -1lF --icons=always --color --long --git -s modified --total-size -o"
-alias l="eza --color -l --git --icons=always --no-time --no-user --no-permissions -s type -s modified --group-directories-first -o"
-alias ll="eza --color -l --git --icons=always --no-time --no-user --no-permissions -s type -s modified --all --group-directories-first -o"
+alias l="eza --color -l --git --icons=always --no-user --no-permissions -s type -s modified --group-directories-first -o --time-style relative"
+alias ll="eza --color -l --git --icons=always --no-user --no-permissions -s type -s modified --all --group-directories-first -o --time-style relative"
 alias lt="eza -1T"
 alias ls="ls -l"
 alias rp="realpath" # get abs path
