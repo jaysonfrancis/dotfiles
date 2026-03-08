448a5ebbc2c6cd5042737421fbfd7bec5235111e
jaysonfranics <jaysonfrancis@gmail.com>
2026-03-08 15:53:03 -0700

    update sketchy

--- config/sketchybar/plugins/aerospace.sh
+++ config/sketchybar/plugins/aerospace.sh
@@ -2,9 +2,10 @@
 
 if [ "$FOCUSED_WORKSPACE" = "$NAME" ] || [ "space.$FOCUSED_WORKSPACE" = "$NAME" ]; then
   sketchybar --set "$NAME" background.drawing=on \
-                           background.color=0xffe1e3e4 \
+                           background.color=0xff7aa2f7 \
                            icon.color=0xff1a1b26
 else
-  sketchybar --set "$NAME" background.drawing=off \
-                           icon.color=0xff888888
+  sketchybar --set "$NAME" background.drawing=on \
+                           background.color=0xe01a1b26 \
+                           icon.color=0xff565f89
 fi
---
+++ config/sketchybar/plugins/volume_click.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+MUTED="$(osascript -e 'output muted of (get volume settings)')"
+
+if [ "$MUTED" = "true" ]; then
+  osascript -e 'set volume output muted false'
+else
+  osascript -e 'set volume output muted true'
+fi
